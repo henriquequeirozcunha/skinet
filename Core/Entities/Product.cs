55c9f36bab0ade72a8c7cf2d00086e0b8409618c
Henrique Queiroz <henrique.cunha@corpus.inf.br>
2020-06-15 22:56:39 -0300

    InitialCOmmit

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,10 @@
+namespace Core.Entities
+{
+    public class Product
+    {
+        // [MaxLength(255)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
