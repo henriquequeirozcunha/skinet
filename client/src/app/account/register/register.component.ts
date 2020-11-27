8ad4b5265c33b53c2afa13d64dd8c6090bc4c7a2
Henrique Cunha <henrique.cunha@corpus.inf.br>
2020-11-27 00:15:19 -0300

    Commit parte do Login Component

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,4 +1,7 @@
+import { Router } from '@angular/router';
+import { AccountService } from './../account.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  createRegisterForm(): void {
+    this.registerForm = this.fb.group({
+      displayName: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]]
+    });
+  }
+
+  onSubmit(): void{
+    console.log(this.registerForm.value);
   }
 
 }
--- client/src/app/shared/components/text-input/text-input.component.ts
+++ client/src/app/shared/components/text-input/text-input.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input, Self } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
 
 @Component({
   selector: 'app-text-input',
   templateUrl: './text-input.component.html',
   styleUrls: ['./text-input.component.scss']
 })
-export class TextInputComponent implements OnInit {
+export class TextInputComponent implements OnInit, ControlValueAccessor {
+  @ViewChild('input', {static: true}) input: ElementRef;
+  @Input() type = 'text';
+  @Input() label: string;
 
-  constructor() { }
+  constructor(@Self() public controlDir: NgControl) {
+    this.controlDir.valueAccessor = this;
+   }
 
   ngOnInit(): void {
+    const control = this.controlDir.control;
+    const validators = control.validator ? [control.validator] : [];
+    const asyncValidator = control.asyncValidator ? [control.asyncValidator] : [];
+
+    control.setValidators(validators);
+    control.setAsyncValidators(asyncValidator);
+    control.updateValueAndValidity();
+  }
+
+  onChanged(event): void{}
+  onThouched(): void {}
+
+  writeValue(obj: any): void {
+    this.input.nativeElement.value = obj || '';
+  }
+  registerOnChange(fn: any): void {
+    this.onChanged = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onThouched = fn;
   }
 
 }
